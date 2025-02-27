0163dff46885fbfe055ea988a7d6d48bceb3b057
ocastrup <ole.christain.astrup@dnv.com>
2025-02-27 16:12:57 +0100

    Bump to 2.0.0

--- ocx_common/__init__.py
+++ ocx_common/__init__.py
@@ -2,6 +2,6 @@
 #  Copyright (c) 2024. OCX Consortium https://3docx.org. See the LICENSE
 from loguru import logger
 
-__version__ = "1.6.0"
+__version__ = "2.0.0"
 
 logger.disable("src.ocx_common")
